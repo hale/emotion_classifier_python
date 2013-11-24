3088cc743c50b57094fd916b18b5ddc36d459d42
Philip Hale <phil989@gmail.com>
2013-11-24 18:32:43 +0000

    Cleanup, small refactorings.

--- classifier.py
+++ classifier.py
@@ -2,10 +2,20 @@
 # -*- coding: utf-8 -*-
 import re, random, math, collections, itertools, pdb
 
-def sentiments():
+# Gives the emotions we are interested in classifying.  To classify an
+# additional emotion, add its name here and an accompanying data file with some
+# training data.
+def SENTIMENTS():
     return ['angry', 'disgusted', 'fearful', 'happy', 'sad', 'surprised']
 
-#  Scott Triglia. Elegant N-gram Generation in Python. (2013-01-20).
+# The Bayes classifier gives a number between 0 and 1, expressing the
+# confidence that the sentence is of a certain sentiment. The threshold
+# determines the required certainty for a sentence to be classified as that
+# sentiment.
+def THRESHOLD():
+    1.0 / float(len(SENTIMENTS()))
+
+#  Adapted from Scott Triglia. Elegant N-gram Generation in Python. (2013-01-20).
 #  URL: http://locallyoptimal.com/blog/2013/01/20/elegant-n-gram-generation-in-python/.
 #  Accessed: 2013-11-21.
 #  Archived by WebCite® at http://www.webcitation.org/6LImC39zN
@@ -15,37 +25,26 @@ def makeNgram(wordList, order):
         ngramList.append('_'.join(ngram))
     return ngramList
 
-#------------ Function Definitions ---------------------
-
 def readFiles(sentencesTrain,sentencesTest):
-
-    #reading pre-labeled movie reviews and splitting into lines
-
-    sentences = {}
-    for sentiment in sentiments():
+    for sentiment in SENTIMENTS():
         txt = open('emotions/' + sentiment + '.txt')
-        sentences[sentiment] = re.split(r'\n', txt.read())
-
-    for sentiment,sentences in sentences.iteritems():
-        for sentence in sentences:
+        for sentence in re.split(r'\n', txt.read()):
             if random.randint(1,10)<2:
                 sentencesTest[sentence] = sentiment
             else:
                 sentencesTrain[sentence] = sentiment
 
-#----------------------------End of data initialisation ----------------#
-
 #calculates p(W|Positive), p(W|Negative) and p(W) for all words in training data
 def trainBayes(sentencesTrain, pWord):
 
     freq = {}
-    for sentiment in sentiments():
+    for sentiment in SENTIMENTS():
         freq[sentiment] = {}
 
     dictionary = {}
 
     wordTotals = { 'all': 0 }
-    for sentiment in sentiments():
+    for sentiment in SENTIMENTS():
         wordTotals[sentiment] = 0
 
     #iterate through each sentence/sentiment pair in the training data
@@ -53,13 +52,13 @@ def trainBayes(sentencesTrain, pWord):
         if sentence == '':
             continue
 
-        wordList = re.findall(r"[\w']+", sentence) # get word list
+        wordList = re.findall(r"[\w']+", sentence)
 
         unigramList = wordList
         bigramList = makeNgram(wordList, 2)
         trigramList = makeNgram(wordList, 3)
 
-        for word in (bigramList + unigramList + trigramList): # now calculate over bigrams
+        for word in (trigramList + bigramList + unigramList):
             wordTotals['all'] += 1
             if not dictionary.has_key(word):
                 dictionary[word] = 1
@@ -70,45 +69,37 @@ def trainBayes(sentencesTrain, pWord):
                 freq[sentiment][word] += 1
 
 
+    # smoothing so min count of each word is 1
     for word in dictionary:
-        #do some smoothing so that minimum count of a word is 1
-        for sentiment in sentiments():
+        for sentiment in SENTIMENTS():
             if not freq[sentiment].has_key(word):
                 freq[sentiment][word] = 1
             else:
                 freq[sentiment][word] += 1
 
+        # divisor for the p(word) calculation
         freqWordAll = 0
-        for sentiment in sentiments():
+        for sentiment in SENTIMENTS():
             freqWordAll += freq[sentiment][word]
 
-        # calculate p(word|sentiment)
-        for sentiment in sentiments():
+        # p(word|sentiment)
+        for sentiment in SENTIMENTS():
             pWord[sentiment][word] = freq[sentiment][word] / float(wordTotals[sentiment])
-        pWord['all'][word] = freqWordAll / float(wordTotals['all']) #p(word)
+
+        #p(word)
+        pWord['all'][word] = freqWordAll / float(wordTotals['all']) 
 
 
-#---------------------------End Training ----------------------------------
-
-#implement naive bayes algorithm
 #INPUTS:
-#  sentences is a dictonary with sentences associated with sentiment
-#  dataName is a string (used only for printing output)
-#  pWordPos is dictionary storing p(word|positive) for each word
-#     i.e., pWordPos["apple"] will return a real value for p("apple"|positive)
-#  pWordNeg is dictionary storing p(word|negative) for each word
-#  pWord is dictionary storing p(word)
-#  pPos is a real number containing the fraction of positive reviews in the dataset
-def testBayes(sentences, dataName, pWord):
-
+#  sentences is a dictonary of { sentence: sentiment } for every sentence.
+#  pWord is dictionary storing p(word) and p(word|sentiment)
+def testBayes(sentences, pWord):
     total = {}
-    total['all'] = 0
-    for sentiment in sentiments():
-        total[sentiment] = 0
-
     correct = {}
+    total['all'] = 0
     correct['all'] = 0
-    for sentiment in sentiments():
+    for sentiment in SENTIMENTS():
+        total[sentiment] = 0
         correct[sentiment] = 0
 
     #for each sentence, sentiment pair in the dataset
@@ -123,60 +114,52 @@ def testBayes(sentences, dataName, pWord):
         trigramList = makeNgram(wordList, 3)
 
         p = {}
-        for s in sentiments():
+        for s in SENTIMENTS():
             p[s] = 0.16
 
         for word in (trigramList + bigramList + unigramList):
             if pWord['all'].has_key(word):
-                for s in sentiments():
+                for s in SENTIMENTS():
                     if pWord[s][word] > 0.00000001:
                         p[s] *= pWord[s][word] * 10000
 
         total['all'] += 1
+        total[sentiment] += 1
 
         totalProb = float(sum(p.itervalues()))
-        threshold = 0.166
         prob = 0
         prob = p[sentiment] / totalProb
-        total[sentiment] += 1
-        if prob >= threshold:
+        if prob >= THRESHOLD():
             correct['all'] += 1
             correct[sentiment] += 1
 
-    print dataName + " Accuracy"
+    accuracy = {}
+    accuracy['all'] = correct['all'] / float(total['all'])
 
-    acc = {}
-    acc['all'] = correct['all'] / float(total['all'])
-    for sentiment in sentiments():
-        acc[sentiment] = correct[sentiment] /  float(total[sentiment])
-
-    print " (ALL)=%0.2f" % acc['all'] + " (%d" % correct['all'] + "/%d" % total['all'] + ")"
-    for sentiment in sentiments():
-        print " (" + sentiment + ")=%0.2f" % acc[sentiment] + " (%d" % correct[sentiment] + "/%d" % total[sentiment] + ")"
-
+    print " (ALL)=%0.2f" % accuracy['all'] + " (%d" % correct['all'] + "/%d" % total['all'] + ")"
+    for sentiment in SENTIMENTS():
+        accuracy[sentiment] = correct[sentiment] /  float(total[sentiment])
+        print " (" + sentiment + ")=%0.2f" % accuracy[sentiment] + " (%d" % correct[sentiment] + "/%d" % total[sentiment] + ")"
 
 #---------- Main Script --------------------------
 
 
+#initialise datasets and dictionaries
 sentencesTrain={}
 sentencesTest={}
+pWord={ 'all': {} }
+for sentiment in SENTIMENTS():
+    pWord[sentiment] = {}
 
-#initialise datasets and dictionaries
+# split the sentiment files into training and test sets
 readFiles(sentencesTrain,sentencesTest)
-
-pWord={ 'all': {} } # p(W)
-for sentiment in sentiments():
-    pWord[sentiment] = {} # p(W|sentiment)
-
-
 
 #build conditional probabilities using training data
 trainBayes(sentencesTrain, pWord)
 
 #run naive bayes classifier on datasets
 print "Naive Bayes"
-testBayes(sentencesTrain, "Train", pWord)
-
-print
-
-testBayes(sentencesTest,  "Test", pWord)
+print "Train Accuracy"
+testBayes(sentencesTrain, pWord)
+print "Test Accuracy"
+testBayes(sentencesTest, pWord)
